de60ef9ef102a551eb77409786fe853e7ddf61dd
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2022-08-14 18:08:56 +0300

    Initial commit

---
+++ Packagedll.bat
@@ -0,0 +1,11 @@
+"C:\MRE_SDK\tools\DllPackage.exe" "D:\MyGitHub\RTXBlocksVxp\RTXBlocksVxp.vcproj"
+if %errorlevel% == 0 (
+ echo postbuild OK.
+  copy RTXBlocksVxp.vpp ..\..\..\MoDIS_VC9\WIN32FS\DRIVE_E\RTXBlocksVxp.vpp /y
+exit 0
+)else (
+echo postbuild error
+  echo error code: %errorlevel%
+  exit 1
+)
+
