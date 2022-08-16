4d258217558b216862623dd7d00fb1704bf5aabd
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2022-08-16 15:26:43 +0300

    Added UDP support

--- Sock.h
+++ Sock.h
@@ -1,7 +1,7 @@
 #pragma once
 
 const int tcp_in_buf_size = 10240*2, tcp_out_buf_size = 10240*2;
-const int upd_in_buf_size = 10240*2, udp_out_buf_size = 10240*2;
+const int udp_in_buf_size = 10240*2, udp_out_buf_size = 10240*2;
 
 namespace Sock
 {
