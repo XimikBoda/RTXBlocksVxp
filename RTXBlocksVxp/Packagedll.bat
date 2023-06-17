44793fb94079faeab903b2aa42a698fb7b8c6ec3
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2023-06-17 13:23:59 +0300

    Some fixes

--- RTXBlocksVxp/Packagedll.bat
+++ RTXBlocksVxp/Packagedll.bat
@@ -1,7 +1,6 @@
 %MRE_SDK%\tools\DllPackage.exe %cd%\RTXBlocksVxp.vcproj
 if %errorlevel% == 0 (
  echo postbuild OK.
-  copy RTXBlocksVxp.vpp ..\..\..\MoDIS_VC9\WIN32FS\DRIVE_E\RTXBlocksVxp.vpp /y
 exit 0
 )else (
 echo postbuild error
